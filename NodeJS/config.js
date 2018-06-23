bb12adf6beb305815393ad3c70c6bc011a59ca8c
KhangPQ <khang.pham@30shine.com>
2018-06-23 11:14:35 +0700

    Update port for deploy

--- NodeJS/config.js
+++ NodeJS/config.js
@@ -3,11 +3,11 @@ module.exports = {
   MONGO_DATABASE_PASSWORD: "admin1",
   MONGO_DATABASE_URL: "@ds261460.mlab.com:61460/iot_final",
   APP_SECRET_KEY: "Khang.Pham",
-  API_PUSH_NOTIC: "http://localhost:3000",
-  API_CLOUD: "http://localhost:3000",
+  API_PUSH_NOTIC: "http://localhost:28888",
+  API_CLOUD: "http://localhost:28888",
   BASE_URL: "localhost",
   TYPE: "http",
-  PORT: "3000",
+  PORT: "28888",
   SWAGGER: {
     APIS_PATH: ['./api/device/*.js', './api/socket/*.js', './api/code/*.js'],
     APIS_TAG: [{
@@ -16,6 +16,10 @@ module.exports = {
     }, {
       name: "Socket",
       description: "Socket server for transmit data"
-    }]
+    }, {
+      name: "Code",
+      description: "APIs for manage Code button"
+    }
+    ]
   }
 };
